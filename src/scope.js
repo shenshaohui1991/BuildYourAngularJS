ff322556983017ee307ff399d52c7c325b01335f
SSH <shenshaohui1991@gmail.com>
2017-03-31 07:58:21 +0800

    $watchGroup

--- src/scope.js
+++ src/scope.js
@@ -180,6 +180,56 @@ Scope.prototype.$$digestOnce = function () {
     return dirty;
 };
 
+Scope.prototype.$watchGroup = function(watchFns, listenerFn) {
+    var self = this,
+        newValues = new Array(watchFns.length),
+        oldValues = new Array(watchFns.length),
+        changeReactionScheduled = false,
+        firstRun = true,
+        shouldCall = true;
+
+    if (watchFns.length === 0) {
+        self.$evalAsync(function () {
+            if (shouldCall) {
+                listenerFn(newValues, newValues, self);
+            }
+        });
+
+        return function () {
+            shouldCall = false;
+        };
+    }
+
+    function watchGroupListener() {
+        if (firstRun) {
+            firstRun = false;
+            listenerFn(newValues, newValues, self);
+        } else {
+            listenerFn(newValues, oldValues, self);
+        }
+
+        changeReactionScheduled = false;
+    }
+
+    var destoryFns = _.map(watchFns, function (watchFn, i) {
+        return self.$watch(watchFn, function (newValue, oldValue) {
+            newValues[i] = newValue;
+            oldValues[i] = oldValue;
+
+            if (!changeReactionScheduled) {
+                changeReactionScheduled = true;
+                self.$evalAsync(watchGroupListener);
+            }
+        });
+    });
+
+    return function() {
+        _.forEach(destoryFns, function (destoryFn) {
+            destoryFn();
+        });
+    };
+};
+
 Scope.prototype.$beginPhase = function (phase) {
     if (this.$$phase) {
         throw this.$$phase + 'already in progress';
--- test/scope.test.js
+++ test/scope.test.js
@@ -668,6 +668,119 @@ describe('Scope', function () {
             scope.$digest();
             expect(scope.counter).to.be.equal(0);
         });
+    });
+
+    describe('$watchGroup', function() {
+        var scope;
+        beforeEach(function () {
+            scope = new Scope();
+        });
+
+        it('takes watches as an array and calls listener with arrays', function () {
+            var gotNewValues, gotOldValues;
+            scope.aValue = 1;
+            scope.anotherValue = 2;
+            scope.$watchGroup([
+                function (scope) {
+                    return scope.aValue;
+                },
+                function (scope) {
+                    return scope.anotherValue;
+                }
+            ], function (newValues, oldValues, scope) {
+                gotNewValues = newValues;
+                gotOldValues = oldValues;
+            });
+            scope.$digest();
+            expect(gotNewValues).to.deep.equal([1, 2]);
+            expect(gotOldValues).to.deep.equal([1, 2]);
+        });
+
+        it('only calls listener once per digest', function() {
+            var counter = 0;
+            scope.aValue = 1;
+            scope.anotherValue = 2;
+            scope.$watchGroup([
+                function(scope) { return scope.aValue; },
+                function(scope) { return scope.anotherValue; }
+            ], function(newValues, oldValues, scope) {
+                counter++;
+            });
+            scope.$digest();
+            expect(counter).to.be.equal(1);
+        });
+
+        it('uses the same array of old and new values on first run', function() {
+            var gotNewValues, gotOldValues;
+            scope.aValue = 1;
+            scope.anotherValue = 2;
+            scope.$watchGroup([
+                function(scope) { return scope.aValue; },
+                function(scope) { return scope.anotherValue; }
+            ], function(newValues, oldValues, scope) {
+                gotNewValues = newValues;
+                gotOldValues = oldValues;
+            });
+
+            scope.$digest();
+            expect(gotNewValues).to.be.equal(gotOldValues);
+        });
+
+        it('uses different arrays for old and new values on subsequent runs', function() {
+            var gotNewValues, gotOldValues;
+            scope.aValue = 1;
+            scope.anotherValue = 2;
+            scope.$watchGroup([
+                function(scope) { return scope.aValue; },
+                function(scope) { return scope.anotherValue; }
+            ], function(newValues, oldValues, scope) {
+                gotNewValues = newValues;
+                gotOldValues = oldValues;
+            });
+            scope.$digest();
+            scope.anotherValue = 3;
+            scope.$digest();
+            expect(gotNewValues).to.deep.equal([1, 3]);
+            expect(gotOldValues).to.deep.equal([1, 2]);
+        });
+
+        it('calls the listener once when the watch array is empty', function() {
+            var gotNewValues, gotOldValues;
+            scope.$watchGroup([], function(newValues, oldValues, scope) {
+                gotNewValues = newValues;
+                gotOldValues = oldValues;
+            });
+            scope.$digest();
+            expect(gotNewValues).to.deep.equal([]);
+            expect(gotOldValues).to.deep.equal([]);
+        });
+
+        it('can be deregistered', function() {
+            var counter = 0;
+            scope.aValue = 1;
+            scope.anotherValue = 2;
+            var destroyGroup = scope.$watchGroup([
+                function(scope) { return scope.aValue; },
+                function(scope) { return scope.anotherValue; }
+            ], function(newValues, oldValues, scope) {
+                counter++;
+            });
+            scope.$digest();
+            scope.anotherValue = 3;
+            destroyGroup();
+            scope.$digest();
+            expect(counter).to.be.equal(1);
+        });
+
+        it('does not call the zero-watch listener when deregistered first', function() {
+            var counter = 0;
+            var destroyGroup = scope.$watchGroup([], function(newValues, oldValues, scope) {
+                counter++;
+            });
+            destroyGroup();
+            scope.$digest();
+            expect(counter).to.be.equal(0);
+        });
         /////////////////////////
     });
 });
